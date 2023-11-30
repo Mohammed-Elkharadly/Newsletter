67ce2321f83aa1653a0dec993d6aada8257b8662
mohammed elkharadly <mkoko201540@gmail.com>
2023-11-30 09:49:05 +0200

    newsletter

---
+++ main.js
@@ -0,0 +1,29 @@
+const container = document.getElementById("container");
+const subscribing = document.getElementById("subscribing");
+const email = document.getElementById("email");
+const btnOne = document.getElementById("btn-one");
+const btnTwo = document.getElementById("btn-two");
+let errorMessage = document.querySelector(".error-message");
+
+let emailRe =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+btnOne.addEventListener("click", function (e) {
+  errorMessage.innerHTML = "";
+  if (!emailRe.test(email.value)) {
+    e.preventDefault();
+    errorMessage.innerHTML = "valid email required";
+    email.style.borderColor = "hsl(4, 100%, 67%)";
+    email.style.color = "hsl(4, 100%, 67%)";
+    email.style.backgroundColor = "hsl(4, 100%, 90%)";
+  } else {
+    container.classList.add("hidden");
+    subscribing.classList.add("show");
+  }
+});
+
+btnTwo.addEventListener("click", function () {
+  subscribing.classList.remove("show");
+  container.classList.add("show");
+  email.value = "";
+});
